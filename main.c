8ff79d85ab508af8d91e29cbd2f9ceafc37f883c
Vasily Davydov <vasily.davydov@metropolia.fi>
2020-12-20 15:09:15 +0200

    Added a little functionality

--- main.c
+++ main.c
@@ -3,10 +3,14 @@
 #include <stdlib.h>
 
 
-int main () {
+int main () 
+{
 
-    int passcode_l, nums_pass [150];
-    const char source []= "1234567890qwertyuiopasdfghjklzxcvbnm#@*QWERTYUIOPASDFGHJKLZXCVBNM";
+    int passcode_l, nums_pass [150], funct;
+    const char source_all []= "1234567890qwertyuiopasdfghjklzxcvbnm#@*QWERTYUIOPASDFGHJKLZXCVBNM";
+    const char source_nums []= "1234567890";
+    const char source_letter []= "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    const char source_letter_nums []= "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 
     FILE*typer;
     if((typer = fopen("/Users/VFilms/Desktop/SavedPasswords/passwords.txt", "r")) == NULL)
@@ -22,15 +26,71 @@ int main () {
 
     system("echo This program will generate your password randomly and write it in a file");
 
-    printf("\n\n Please, enter the lenght of your passcode: ");
-    scanf("%d", &passcode_l);
-    printf("\nYour password is: ");
-    for(int i = 0; i < passcode_l; i++)
+
+    printf("Please tell the program, which password you want to generate:\n\n");
+    printf("1. Password with letters only\n");
+    printf("2. Password with numbers only\n");
+    printf("3. Password with letters and numbers\n");
+    printf("4. Password with symbols, letters and numbers\n\n Enter your choice: ");
+    scanf("%d", &funct);
+
+
+    switch(funct)
+  {
+    case 1:
     {
-      printf("%c", source[rand()%67]);
+        printf("\n\n Please, enter the lenght of your passcode: ");
+        scanf("%d", &passcode_l);
+        printf("\nYour password is: ");
+        for(int i = 0; i < passcode_l; i++)
+        {
+          printf("%c", source_letter[rand()%54]);
+        }
+        printf("\n");
+        break;
     }
-    printf("\n");
-  
+    case 2:
+    {
+        printf("\n\n Please, enter the lenght of your passcode: ");
+        scanf("%d", &passcode_l);
+        printf("\nYour password is: ");
+        for(int i = 0; i < passcode_l; i++)
+        {
+          printf("%c", source_nums[rand()%11]);
+        }
+        printf("\n");
+        break;
+    }
+    case 3:
+    {
+        printf("\n\n Please, enter the lenght of your passcode: ");
+        scanf("%d", &passcode_l);
+        printf("\nYour password is: ");
+        for(int i = 0; i < passcode_l; i++)
+        {
+          printf("%c", source_letter_nums[rand()%64]);
+        }
+        printf("\n");
+        break;
+    }
+    case 4:
+    {
+        printf("\n\n Please, enter the lenght of your passcode: ");
+        scanf("%d", &passcode_l);
+        printf("\nYour password is: ");
+        for(int i = 0; i < passcode_l; i++)
+        {
+          printf("%c", source_all[rand()%67]);
+        }
+        printf("\n");
+        break;
+    }
+    default:
+    {
+      printf("Your input is wrong");
+      
+    }
+  }
 
 
 
