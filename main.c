fdb7b04779714fb63053c7b8d76046618ec9cba9
Vasily Davydov <vasily.davydov@metropolia.fi>
2020-12-20 14:40:34 +0200

    Checks for a file
    Checks if the file with passwords is created

--- main.c
+++ main.c
@@ -8,6 +8,17 @@ int main () {
     int passcode_l, nums_pass [150];
     const char source []= "1234567890qwertyuiopasdfghjklzxcvbnm#@*QWERTYUIOPASDFGHJKLZXCVBNM";
 
+    FILE*typer;
+    if((typer = fopen("/Users/VFilms/Desktop/SavedPasswords/passwords.txt", "r")) == NULL)
+    {
+      printf("File is not created or program failed to open it.");
+    } else
+    {
+      printf("File is found.\n");
+    }
+    
+
+
 
     system("echo This program will generate your password randomly and write it in a file");
 
@@ -19,7 +30,7 @@ int main () {
       printf("%c", source[rand()%67]);
     }
     printf("\n");
-    
+  
 
 
 
